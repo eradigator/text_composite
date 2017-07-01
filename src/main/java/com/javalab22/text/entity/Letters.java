e6e5e04b176183d197dc1bf57965ea1e5204429e
eradigator <eradigator@gmail.com>
2017-07-01 13:00:37 +0600

    decomposition ready

--- src/main/java/com/javalab22/text/entity/Letters.java
+++ src/main/java/com/javalab22/text/entity/Letters.java
@@ -1,7 +1,14 @@
 package com.javalab22.text.entity;
 
+import com.javalab22.text.operation.Splitter;
+
+import java.util.List;
+
 /**
  * Created by vten on 30.06.2017.
  */
-public class Letters {
+public class Letters implements Splitter {
+    public List<String> split(List<String> list) {
+        return null;
+    }
 }
--- src/main/java/com/javalab22/text/runner/Runner.java
+++ src/main/java/com/javalab22/text/runner/Runner.java
@@ -1,5 +1,6 @@
 package com.javalab22.text.runner;
 
+import com.javalab22.text.entity.Paragraph;
 import com.javalab22.text.entity.Text;
 import com.javalab22.text.entity.Sentence;
 import com.javalab22.text.entity.WordAndPunctuation;
@@ -8,6 +9,7 @@ import com.javalab22.text.util.InputFromFile;
 import com.javalab22.text.util.OutputToFile;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,22 +22,22 @@ public class Runner {
     public static void main(String[] args) {
 
         Text text = new Text();
-        InputFromFile in = new InputFromFile();
-        text.setText(in.readFile("text.txt"));
-        System.out.println(text.getText());
-
-        OutputToFile out = new OutputToFile();
-        out.outputToFile(text);
-
+        text.addText(new InputFromFile().readFile("text.txt"));
 
         Print print = new Print();
+        print.printArrayList(text.getText());
 
-        /*
-        List<String> sentences = (new Sentence()).split(text.getText());
+        List<String> paragraphs = new Paragraph().split(text.getText());
+        print.printArrayList(paragraphs);
+
+        List<String> sentences = (new Sentence()).split(paragraphs);
         print.printArrayList(sentences);
 
         List<String> words = (new WordAndPunctuation().split(sentences));
-        print.printArrayList(words);*/
+        print.printArrayList(words);
+
+        OutputToFile out = new OutputToFile();
+        out.outputToFile(text);
 
     }
 }
