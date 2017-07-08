c2cea50b2e6fe725fb575f4dddde2c3b5c41a90f
erad <eradigator@gmail.com>
2017-07-08 15:01:11 +0600

    some adds

---
+++ src/main/java/com/javalab22/text/entity/TypeOfTextComponent.java
@@ -0,0 +1,8 @@
+package com.javalab22.text.entity;
+
+/**
+ * Created by erad on 08.07.2017.
+ */
+public enum TypeOfTextComponent {
+    TEXT, PARAGRAPH, SENTENCE, SENTENCE1, WORD, SYMBOL;
+}
--- src/main/java/com/javalab22/text/entity/impl/Symbol.java
+++ src/main/java/com/javalab22/text/entity/impl/Symbol.java
@@ -1,6 +1,7 @@
 package com.javalab22.text.entity.impl;
 
 import com.javalab22.text.entity.TextComponent;
+import com.javalab22.text.entity.TypeOfTextComponent;
 
 /**
  * Created by erad on 02.07.2017.
@@ -12,8 +13,26 @@ public class Symbol implements TextComponent {
 
     private String symbol;
 
+    private TypeOfTextComponent typeOfTextComponent;
+
     public Symbol(String symbol) {
         this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public TypeOfTextComponent getTypeOfTextComponent() {
+        return typeOfTextComponent;
+    }
+
+    public void setTypeOfTextComponent(TypeOfTextComponent typeOfTextComponent) {
+        this.typeOfTextComponent = typeOfTextComponent;
     }
 
     public void add(TextComponent textComposite) {
